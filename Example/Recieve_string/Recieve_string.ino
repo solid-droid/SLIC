e11e7c7342cc6cb0258d71f647bc91743327d2d8
Nikhil M Jeby <32586986+solid-droid@users.noreply.github.com>
2018-06-24 10:45:42 +0530

    Add files via upload

---
+++ Example/Recieve_string/Recieve_string.ino
@@ -0,0 +1,21 @@
+//Receive string example for SLIC.
+//Created by Nikhil M Jeby
+//https://github.com/solid-droid
+/////////////////////////////////////////////////////////////////////
+
+#include "SLIC.h"
+slic port(8);         // define a GPIO pin
+void setup() 
+{
+  Serial.begin(9600); 
+  Serial.println("start");
+}
+
+void loop() 
+{
+port.read_if_available();           //reads date if available on the pin.
+if(port.buff_exist)                 // If data is received buff_exist turns HIGH.
+Serial.println(port.get_string());  // get_string() returns received string (data type: String) 
+                                    //and sets buff_exist to LOW
+}
+
---
+++ Example/Transmit_string/Transmit_string.ino
@@ -0,0 +1,28 @@
+//Transmit string example for SLIC.
+//Created by Nikhil M Jeby
+//https://github.com/solid-droid
+/////////////////////////////////////////////////////////////////////
+
+#include "SLIC.h"
+slic port(8);            // define a GPIO port
+char s[]="hello world";  // String to transmit, don’t use String data type.
+int ss;                  // Integer to store string size
+void setup() 
+{
+port.sync_out();        // Sends out synching information. (One time only, on start up of receivers)
+ ss=sizeof(s);          // Size of the string.
+port.write_dat(s,ss);   // Transmit string data (string, string size)
+  
+}
+
+void loop() 
+{
+
+}
+
+
+
+
+
+
+
